e87af55ec878532e9bc2046ded8214e90a6435f0
tmb <tom.brooke@gmail.com>
2021-11-28 17:38:27 -0500

    Functions to pull templates from disk

--- src/aplib/core.cljs
+++ src/aplib/core.cljs
@@ -1,13 +1,70 @@
-(ns aplib.core
-(:require  ["@accordproject/cicero-core"   :as cc]
-           ["@accordproject/cicero-engine" :as ce]
-           ["@accordproject/concerto-core" :as co]))
+(ns aplib.core 
+  (:require [applied-science.js-interop :as j]
+            [promesa.core :as p]
+            ["@accordproject/cicero-core"   :as cc]
+            ["@accordproject/cicero-engine" :as ce]
+            ["@accordproject/concerto-core" :as co]))
 
 
-(def clause (.cc/new Clause))
+(def local-dir "/Users/tmb/Coding/trustblocks/cicero-template-library/src/")
 
-(println "Hello world!")
 
+(defn node-slurp [path]
+  (let [fs (js/require "fs")]
+    (.readFileSync fs path "utf8")))
+
+(defn get-sample
+  "Load sample md from file"
+  [k]
+  (node-slurp (str local-dir k "/text/sample.md")))
+
+ (println (get-template "latedeliveryandpenalty"))
+
+(defn get-template
+  [k]
+  (node-slurp (str local-dir k "/text/grammar.tem.md")))
+
+(defn get-model
+  [k]
+  (node-slurp (str local-dir k "/model/clause.cto")))
+
+(defn get-logic
+  [k]
+  (node-slurp (str local-dir k "/logic/logic.ergo")))
+
+(defn get-metadata
+  [k]
+  (node-slurp (str local-dir k "/package.json")))
+
+(def template (get-template "latedeliveryandpenalty"))
+
+(def clause (get-sample "latedeliveryandpenalty"))
+
+(def model (get-model "latedeliveryandpenalty"))
+
+
+
+
+(def metadata (get-metadata "latedeliveryandpenalty" )) 
+ 
+(def logic (get-logic "latedeliveryandpenalty" )) 
+
+(println template) 
+ 
+(println clause)
+ 
+(println model)
+
+(println metadata)
+
+(println logic)
+
+
+(def modelManager (.new co/ModelManager))
+
+(.addModelFile model (.new ModelManager))
+
+(.cc/parse clause_instance clause)
 
 (comment
 
@@ -15,8 +72,55 @@
 
 ;;;; https://docs.accordproject.org/docs/ref-cicero-api.html 
   
+;;;;; Archive handlers  ;;;;;
+compositeArchiveLoader.accepts (url)
+  
+compositeArchiveLoader.load (url, options) ⇒ Promise
+  
+;;;;; Clauses ;;;;;;
 
+Clause
+  
+A Clause is executable business logic, linked to a natural language (legally enforceable) 
+  template. A Clause must be constructed with a template and then prior to execution the data 
+  for the clause must be set. Set the data for the clause (an instance of the template model) 
+  by either calling the setData method or by calling the parse method and passing in natural 
+  language text that conforms to the template grammar.
+
+Templates
+
+templateInstance.draft ([options], [currentTime], [utcOffset]) ⇒ string
+Generates the natural language text for a contract or clause clause; combining the text from the template and the instance data.
+
+
+templateInstance.toJSON() ⇒ object
+Returns a JSON representation of the clause
+
+Kind: instance method of TemplateInstance
+Returns: object - the JS object for serialization
   const template = await Template.fromDirectory('./test/data/latedeliveryandpenalty') ;
+
+
+TemplateInstance.rebuildParser(parserManager, logicManager, ergoEngine, templateName, grammar)
+Utility to rebuild a parser when the grammar changes
+
+Kind: static method of TemplateInstance
+
+Param	Type	Description
+parserManager	*	the parser manager
+logicManager	*	the logic manager
+ergoEngine	*	the evaluation engine
+templateName	string	this template name
+grammar	string	the new grammar
+
+
+template.getParserManager() ⇒ ParserManager
+Provides access to the parser manager for this template. The parser manager can convert template data to and from natural language text.
+
+Kind: instance method of Template
+Returns: ParserManager - the ParserManager for this template
+
+
 
 // load the DSL text for the template
 
@@ -27,6 +131,7 @@
 
 // get the JSON object created from the parse
 
+  (println clause)  
     const data = clause.getData ();
   )
 
